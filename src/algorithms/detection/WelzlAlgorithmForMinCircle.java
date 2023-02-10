110e0e797629a1107d6913b4d946b6d2da6959fc
waradia <aboubacardiawara909@gmail.com>
2023-02-10 22:04:49 +0100

    implementation partielle de welzl

--- src/algorithms/detection/WelzlAlgorithmForMinCircle.java
+++ src/algorithms/detection/WelzlAlgorithmForMinCircle.java
@@ -20,21 +20,39 @@ public class WelzlAlgorithmForMinCircle extends Algorithm {
 		ArrayList<Point> points = (ArrayList<Point>) inputPoints.clone();
 		if (points.isEmpty()) {
 			return null;
-		} 
-		
+		}
+
 		Point p = points.get(0);
 		points.remove(p);
 		Circle d = minidisk(points);
-		if (! circleContainsPoint(d, p)) {
+		if (!circleContainsPoint(d, p)) {
 			ArrayList<Point> r = new ArrayList<>();
-			r.add(p);	
+			r.add(p);
 			d = b_minidisk(points, r);
 		}
-		
+
 		return d;
 	}
 
-	private Circle b_minidisk(ArrayList<Point> points, ArrayList<Point> r) {
+	private Circle b_minidisk(ArrayList<Point> inputPoints, ArrayList<Point> r) {
+		ArrayList<Point> points = (ArrayList<Point>) inputPoints.clone();
+		Circle d;
+		if (r.size() == 3 || points.isEmpty()) {
+			d = dhandleBaseCase(r);
+		}
+
+		Point p = points.get(0);
+
+		d = b_minidisk(points, r);
+		if (d != null && !circleContainsPoint(d, p)) {
+			r.add(p);
+			d = b_minidisk(points, r);
+		}
+		return null;
+	}
+
+	private Circle dhandleBaseCase(ArrayList<Point> r) {
+		// TODO Auto-generated method stub
 		return null;
 	}
 }
