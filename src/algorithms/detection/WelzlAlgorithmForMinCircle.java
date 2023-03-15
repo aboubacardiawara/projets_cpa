335d929295e31527aeb9d3f56a899b8c0c078bb4
waradia <aboubacardiawara909@gmail.com>
2023-03-15 18:06:15 +0100

    trying to run !

--- src/algorithms/detection/WelzlAlgorithmForMinCircle.java
+++ src/algorithms/detection/WelzlAlgorithmForMinCircle.java
@@ -1,8 +1,6 @@
 package algorithms.detection;
 
 import java.awt.Point;
-import java.lang.invoke.StringConcatFactory;
-import java.nio.channels.NonWritableChannelException;
 import java.util.ArrayList;
 
 import supportGUI.Circle;
@@ -15,87 +13,115 @@ public class WelzlAlgorithmForMinCircle extends Algorithm {
 	}
 
 	private Circle minidisk(ArrayList<Point> inputPoints) {
-		/**
-		 * if P = ~ then D : = ~ else choose p 6 P; D := m i n i d i s k ( P - {p}); if
-		 * p ~ D then D := b_minidisk(P - {p},p)
-		 */
-		@SuppressWarnings("unchecked")
+		if (inputPoints.isEmpty()) {
+			return null;
+		}
+		ArrayList<Point> points = new ArrayList<>(inputPoints);
+		Circle c = wikipediaTrial(points, new ArrayList<>());
+		assertionCheck(c != null, "cercle cannot be null");
+		System.out.println("cercle: (" + c.getCenter() + ",  " + c.getRadius() + ")");
+		return c;
+	}
+
+	@SuppressWarnings("unchecked")
+	private Circle wikipediaTrial(ArrayList<Point> inputPoints, ArrayList<Point> inputR) {
+		ArrayList<Point> R = (ArrayList<Point>) inputR.clone();
 		ArrayList<Point> points = (ArrayList<Point>) inputPoints.clone();
-		Circle d;
+
+		assertionCheck(R.size() <= 3, "Precondition (WelzlAlgorithm): |R| ≤ 3");
+
+		if (points.isEmpty() || R.size() == 3) {
+			return trivial(R);
+		}
+
+		Point p = points.remove(0);
+		Circle D = wikipediaTrial(points, R);
+		if (circleContainsPoint(D, p)) {
+			return D;
+		}
+
+		R.add(p);
+		return wikipediaTrial(points, R);
+	}
+
+	private Circle trivial(ArrayList<Point> r) {
+		if (r.size() == 3) { // |R|=3 case
+			return cercleMinimalTroisPoints(r);
+		} else { // points.isEmpty()
+			return new Circle(new Point(), 0);
+		}
+	}
+
+	private Circle minidiskRec(ArrayList<Point> points, ArrayList<Point> r) {
+		if (r.size() == 3) {
+			return handleBaseCase(r);
+		}
 		if (points.isEmpty()) {
-			d = new Circle(new Point(30, 30), 0);
-		} else {
-			Point p = points.remove(0);
-			d = minidisk(points);
-			assertionCheck(d != null, "circle cannot be null");
-			if (!circleContainsPoint(d, p)) {
-				ArrayList<Point> r = new ArrayList<Point>();
-				r.add(p);
-				d = b_minidisk(points, r);
-			}
+			return new Circle(new Point(), 0);
 		}
-
-		return d;
-	}
-	
-	private void assertionCheck(boolean b, String msg) {
-		if (!b) throw new AssertionError(msg, null);
-	}
-
-	private Circle b_minidisk(ArrayList<Point> inputPoints, ArrayList<Point> r) {
-		ArrayList<Point> points = (ArrayList<Point>) inputPoints.clone();
-		Circle d;
-		if (r.size() == 3 || points.isEmpty()) {
-			d = handleBaseCase(r);
-		}
-
-		Point p = points.get(0);
-
-		d = b_minidisk(points, r);
+		Point p = points.remove(0);
+		Circle d = minidiskRec(points, r);
 		if (d != null && !circleContainsPoint(d, p)) {
 			r.add(p);
-			d = b_minidisk(points, r);
+			d = minidiskRec(points, r);
+			r.remove(p);
+		} else if (d == null) {
+			r.add(p);
+			d = minidiskRec(points, r);
+			r.remove(p);
 		}
 		return d;
 	}
 
-	public Circle handleBaseCase(ArrayList<Point> points) {
-		assert points.size() >= 3;
-		Point A = points.get(0);
-		Point B = points.get(1);
-		Point C = points.get(2);
-
-		double x1 = A.getX();
-		double y1 = A.getY();
-		double x2 = B.getX();
-		double y2 = B.getY();
-		double x3 = C.getX();
-		double y3 = C.getY();
-
-		Point M = center(A, B);
-		Point N = center(B, C);
-
-		// mediatrice A B
-		double a = -(x2 - x1) / (y2 - y1);
-		double b = M.getY() - a * M.getX();
-		// mediatrice B C
-		double c = -(x3 - x2) / (y3 - y2);
-		double d = N.getY() - c * N.getX();
-
-		// intersection
-		int x = (int) ((d - b) / (a - c));
-		int y = (int) (a * x + b);
-		Point intersection = new Point(x, y);
-
-		return new Circle(intersection, (int) intersection.distance(A));
+	private Circle handleBaseCase(ArrayList<Point> r) {
+		if (r.size() == 1) {
+			return new Circle(r.get(0), 0);
+		} else if (r.size() == 2) {
+			return new Circle(center(r.get(0), r.get(1)), (int) (r.get(0).distance(r.get(1)) / 2));
+		} else { // r.size() == 3
+			return circleFromThreePoints(r);
+		}
 	}
-	
-	public Circle handleBaseCaseBis(ArrayList<Point> points) {
-		Line diametre = calculDiametre(points);
-		Point p = diametre.getP();
-		Point q = diametre.getQ();
-		Point c = center(p, q);
-		
-		return new Circle(c, (int) c.distance(p));
+
+	private Circle circleFromThreePoints(ArrayList<Point> points) {
+		Line l = calculDiametre(points);
+		Point p = l.getP();
+		Point q = l.getQ();
+		Point m = midPoint(p, q);
+		return new Circle(m, (int) p.distance(q));
 	}
+
+	private Point midPoint(Point p, Point q) {
+		int x = (int) ((p.getX() + q.getX()) / 2);
+		int y = (int) ((p.getY() + q.getY()) / 2);
+		return new Point(x, y);
+	}
+
+	private void assertionCheck(boolean b, String msg) {
+		if (!b)
+			throw new AssertionError(msg, null);
+	}
+
+	private Circle cercleMinimalTroisPoints(ArrayList<Point> points) {
+		Point a = new Point((int) points.get(0).getX(), (int) points.get(0).getY());
+		Point b = new Point((int) points.get(1).getX(), (int) points.get(1).getY());
+		Point c = new Point((int) points.get(2).getX(), (int) points.get(2).getY());
+		double dA = a.distanceSq(b);
+		double dB = b.distanceSq(c);
+		double dC = c.distanceSq(a);
+		Point center = null;
+		double radius = 0;
+		if (dA > dB && dA > dC) {
+			center = midPoint(a, b);
+			radius = a.distance(center);
+		} else if (dB > dC) {
+			center = midPoint(b, c);
+			radius = b.distance(center);
+		} else {
+			center = midPoint(c, a);
+			radius = c.distance(center);
+		}
+		return new Circle(center, (int) radius);
+	}
+
 }
